7a241f8e9a34ab6c1e5cc9ae8275ef1326a8de89
Maurice Boothe Jr <boothemjr@users.noreply.github.com>
2021-04-06 23:28:25 -0400

    add first dot placement

---
+++ Sticks and Stones/Assets/Scripts/GameManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject dotPrefab;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        DrawGrid();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    // Draw the grid of dots
+    void DrawGrid()
+    {
+        var dot = Instantiate(dotPrefab);
+    }
+}
